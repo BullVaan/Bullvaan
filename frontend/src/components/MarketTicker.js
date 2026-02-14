8d24faaaa23898b229a6dd8a4982533478aab193
Pankaj Sharma <pankajgitjpr2014@gmail.com>
2026-02-14 18:48:16 +0100

    Added Live indexes data from trading view and market close or open with time

---
+++ frontend/src/components/MarketTicker.js
@@ -0,0 +1,33 @@
+import { useEffect } from "react";
+
+function MarketTicker() {
+  useEffect(() => {
+    const container = document.getElementById("ticker");
+
+    container.innerHTML = "";
+
+    const script = document.createElement("script");
+    script.src =
+      "https://s3.tradingview.com/external-embedding/embed-widget-ticker-tape.js";
+    script.async = true;
+
+    script.innerHTML = JSON.stringify({
+      symbols: [
+        { proName: "NSE:NIFTY50", title: "NIFTY 50" },
+        { proName: "NSE:BANKNIFTY1!", title: "BANK NIFTY" },
+        { proName: "BSE:SENSEX", title: "SENSEX" }
+      ],
+      showSymbolLogo: true,
+      isTransparent: false,
+      displayMode: "adaptive",
+      colorTheme: "dark",
+      locale: "en"
+    });
+
+    container.appendChild(script);
+  }, []);
+
+  return <div id="ticker"></div>;
+}
+
+export default MarketTicker;
